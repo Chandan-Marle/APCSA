0197af89ee1aa03fe7ee99a6367078d02358e64d
Chandan Marle <chandanmarlex12@gmail.com>
2020-11-02 10:46:33 -0700

    Change varibales on Color Challenge

--- Challenges/Color/ColorTester.java
+++ Challenges/Color/ColorTester.java
@@ -1,9 +1,9 @@
 public class ColorTester {    
     public static void main(String[] args) {
         Color c1 = new Color();
-        int r = c1.redCreation();
-        int g = c1.greenCreation();
-        int b = c1.blueCreation();
-        System.out.println("R:" + r + " G:" + g + " B:" + b);
+        int red = c1.redCreation();
+        int green = c1.greenCreation();
+        int blue = c1.blueCreation();
+        System.out.println("R:" + red + " G:" + green + " B:" + blue);
     }
 }
